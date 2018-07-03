b67c02528ce99f7df4c5e6c634158d2c3bc7c15b
airenelias <airenilia@gmail.com>
2018-07-03 19:23:36 +0700

    Checked multiple file compilation

---
+++ variables/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "variables.h"
+int main()
+{
+	printf("Result = %d",add(2,2));
+	return 0;
+}
---
+++ variables/variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int e = 6;
+int add(int a, int b)
+{
+	static int i;
+        i = i+1;
+	printf("Iteration = %d\n",i);
+	return a+b;
+}
