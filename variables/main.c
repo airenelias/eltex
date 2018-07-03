ee3836f335c31f58e9788243647eba44affc4857
airenelias <airenilia@gmail.com>
2018-07-03 19:31:15 +0700

    Checked extern variables

--- variables/main.c
+++ variables/main.c
@@ -1,9 +1,9 @@
 #include "variables.h"
 int main()
 {
-	int e;
+	extern int e;
 	printf("Extern variable = %d\n",e);
 	for(int i=0;i<10;i=i+1)
-	printf("Result = %d\n",add(2,2));
+	printf("Result = %d\n",add(i,i));
 	return 0;
 }
