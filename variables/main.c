b74bbaca6bf3acc24e24a9b0957c827d871c9240
airenelias <airenilia@gmail.com>
2018-07-03 19:29:39 +0700

    Modified header

--- variables/main.c
+++ variables/main.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
 #include "variables.h"
 int main()
 {
+	int e;
+	printf("Extern variable = %d\n",e);
 	for(int i=0;i<10;i=i+1)
-	printf("Result = %d",add(2,2));
+	printf("Result = %d\n",add(2,2));
 	return 0;
 }
--- variables/variables.h
+++ variables/variables.h
@@ -1,1 +1,2 @@
+#include <stdio.h>
 int add(int,int);
