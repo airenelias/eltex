e398491f295279b86f7cd5bc39a48a7a10324712
airenelias <airenilia@gmail.com>
2018-07-09 17:46:53 +0700

    Added minor modifications

--- complex_calculator/calc.c
+++ complex_calculator/calc.c
@@ -27,21 +27,23 @@ int main()
 {
 	char operation;
 	float re=0; float im=0;
-	float A[2] = {0,0};
-	struct complex *number = &A;
-	printf("Input format: \"Operation Real Imaginary\", q to exit.\n");
+	struct complex number = {0,0};
+	struct complex *ptr = &number;
+	//float A[2] = {0,0};
+	//struct complex *ptr = &A;
+	printf("Input format: \"Operation Real Imaginary\", q to exit.\nExamples: \"+ 2 2\" or \"* 3 4\".\n");
 	while(1)
 	{
-		printf("%f+%fi\n", number->re,number->im);
+		printf("%f+%fi\n", ptr->re,ptr->im);
 		scanf(" %c",&operation);
 		if(operation=='q') break;
 		scanf(" %f %f",&re,&im);
 		switch(operation)
 		{
-			case '+': add(number,re,im); break;
-			case '-': sub(number,re,im); break;
-			case '*': mul(number,re,im); break;
-			case '/': div(number,re,im); break;
+			case '+': add(ptr,re,im); break;
+			case '-': sub(ptr,re,im); break;
+			case '*': mul(ptr,re,im); break;
+			case '/': div(ptr,re,im); break;
 
 			default: break;
 		}
